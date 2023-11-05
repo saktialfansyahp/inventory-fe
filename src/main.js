82b9576ea2c1410e816344f9c76fe3399657c571
saktialfansyahp <saktialfansyahp@gmail.com>
2023-11-06 00:47:56 +0700

    change baseURL with package-lock

--- src/main.js
+++ src/main.js
@@ -9,7 +9,8 @@ import "./assets/css/app.css";
 
 // loadFonts();
 
-axios.defaults.baseURL = "https://inventory-cv2o63cxt-saktialfansyahp.vercel.app/api";
+axios.defaults.baseURL =
+  "https://inventory-cv2o63cxt-saktialfansyahp.vercel.app/api";
 
 // Set header dari localStorage
 // const token = localStorage.getItem("token"); // Ganti 'token' dengan kunci yang Anda gunakan untuk menyimpan token di localStorage
